3e5b66eaa744d5d4b659af3efbedf46d3a7f889f
McL <mcl@brm>
2018-08-09 10:38:18 +0300

    logo postition is set programmatically

--- credit-advisor/creditAdvisor/creditAdvisor/Controller/MainVC.swift
+++ credit-advisor/creditAdvisor/creditAdvisor/Controller/MainVC.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    
+    @IBOutlet weak var logoLabelA: UILabel!
+    @IBOutlet weak var logoLabelB: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        logoLabelA.frame = CGRect(x: view.frame.size.width / 2 - logoLabelA.frame.size.width, y: 40, width: logoLabelA.frame.size.width, height: logoLabelA.frame.size.height)
+        logoLabelB.frame = CGRect(x: view.frame.size.width / 2, y: 55, width: logoLabelB.frame.size.width, height: logoLabelB.frame.size.height)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,5 @@ class MainVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
