e6f16ea00d94de62fef0ffd3b611a1f3a60ce9a5
McL <mcl@brm>
2018-08-09 12:35:42 +0300

    picker experiments

--- credit-advisor/creditAdvisor/creditAdvisor/Controller/MainVC.swift
+++ credit-advisor/creditAdvisor/creditAdvisor/Controller/MainVC.swift
@@ -8,20 +8,40 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
 
     
     @IBOutlet weak var logoLabelA: UILabel!
     @IBOutlet weak var logoLabelB: UILabel!
     
+    @IBOutlet weak var pickerMonths: UIPickerView!
+    var pickerMonthsData: [Int] = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        logoLabelA.frame = CGRect(x: view.frame.size.width / 2 - logoLabelA.frame.size.width, y: 40, width: logoLabelA.frame.size.width, height: logoLabelA.frame.size.height)
-        logoLabelB.frame = CGRect(x: view.frame.size.width / 2, y: 55, width: logoLabelB.frame.size.width, height: logoLabelB.frame.size.height)
+        logoPixelPerfect()
+        setPickerMonths()
+        
+        self.pickerMonths.delegate = self
+        self.pickerMonths.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func setPickerMonths()
+    {
+        pickerMonthsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12]
+        
+        // Connect data:
+        
+    }
 
+    func logoPixelPerfect()
+    {
+        logoLabelA.frame = CGRect(x: view.frame.size.width / 2 - logoLabelA.frame.size.width, y: 40, width: logoLabelA.frame.size.width, height: logoLabelA.frame.size.height)
+        logoLabelB.frame = CGRect(x: view.frame.size.width / 2, y: 55, width: logoLabelB.frame.size.width, height: logoLabelB.frame.size.height)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
