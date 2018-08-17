0617284fc7c455f2b0f15ebae1c17c4a6dbefd3e
McL <mcl@brm>
2018-08-17 08:33:31 +0300

    money of the world app: initial commit

---
+++ money-of-the-world/money-of-the-world/Controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  money-of-the-world
+//
+//  Created by McL on 8/17/18.
+//  Copyright © 2018 McL. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ money-of-the-world/money-of-the-worldTests/money_of_the_worldTests.swift
@@ -0,0 +1,36 @@
+//
+//  money_of_the_worldTests.swift
+//  money-of-the-worldTests
+//
+//  Created by McL on 8/17/18.
+//  Copyright © 2018 McL. All rights reserved.
+//
+
+import XCTest
+@testable import money_of_the_world
+
+class money_of_the_worldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
