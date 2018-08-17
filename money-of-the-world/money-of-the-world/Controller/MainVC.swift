39c47f9577d186e2dd32a16237b205ffabc89e02
McL <mcl@brm>
2018-08-17 10:14:31 +0300

    table view works, prototype cell of country: label fix

--- money-of-the-world/money-of-the-world/Controller/MainVC.swift
+++ money-of-the-world/money-of-the-world/Controller/MainVC.swift
@@ -13,19 +13,14 @@ class MainVC:
             UITableViewDelegate,
             UITableViewDataSource
 {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-
     @IBOutlet weak var countriesTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countriesTable.dataSource = self
+        countriesTable.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -34,6 +29,19 @@ class MainVC:
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCountries().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell") as? CountryCell
+        {
+            let country = DataService.instance.getCountries()[indexPath.row]
+            cell.updateViews(country: country)
+            return cell
+        }
+        return CountryCell()
+    }
 
 }
 
