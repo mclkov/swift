27c31d212b351fc764d38208f0a002d30c0be4b8
McL <mcl@brm>
2018-08-17 11:42:16 +0300

    Country model: added shortName

--- money-of-the-world/money-of-the-world/Model/Country.swift
+++ money-of-the-world/money-of-the-world/Model/Country.swift
@@ -10,14 +10,17 @@ import Foundation
 struct Country
 {
     private(set) public var name: String
+    private(set) public var shortName: String
     private(set) public var imageName: String
     
     init(
+        shortName: String,
         name: String,
         imageName: String
     )
     {
         self.name = name
+        self.shortName = shortName
         self.imageName = imageName
     }
 }
--- money-of-the-world/money-of-the-world/Service/DataService.swift
+++ money-of-the-world/money-of-the-world/Service/DataService.swift
@@ -11,9 +11,24 @@ import Foundation
 class DataService
 {
     static let instance = DataService()
+
     private let countries = [
-        Country(name: "United States of America", imageName: "usa.jpg"),
-        Country(name: "European Union", imageName: "eu.jpg")
+        Country(shortName: "usa", name: "United States of America", imageName: "usa.jpg"),
+        Country(shortName: "eu", name: "European Union", imageName: "eu.jpg")
+    ]
+    
+    private let currencies = [Currency]()
+    
+    private let usaCurrencies = [
+        Currency(name: "1 Dollar", imageName: "usa_1.jpg"),
+        Currency(name: "2 Dollars", imageName: "usa_2.jpg"),
+        Currency(name: "5 Dollars", imageName: "usa_5.jpg")
+    ]
+    
+    private let euCurrencies = [
+        Currency(name: "1 Euro", imageName: "eu_1.jpg"),
+        Currency(name: "2 Euros", imageName: "eu_2.jpg"),
+        Currency(name: "5 Euros", imageName: "eu_5.png")
     ]
     
     func getCountries() -> [Country]
